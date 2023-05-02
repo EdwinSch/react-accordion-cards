5ec56ca6b3e66b7cda77243a46cfd1692d3c8ec6
front-ed <edwinschutjes@gmail.com>
2023-05-02 11:35:04 +0200

    add: toggle functionality and styling

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,13 +1,27 @@
 import content from "../content";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+
+import { useState } from "react";
 
 const Questions = () => {
+  const [showInfo, setShowInfo] = useState(false);
+
   return (
     <section className="questions-container">
       {content.map((item) => {
         const { id, heading, info } = item;
         return (
           <article key={id} className="single-question">
-            {heading}
+            <div className="heading-wrapper">
+              <h2 onClick={() => setShowInfo(!showInfo)}>{heading}</h2>
+              <button
+                className="chevron"
+                onClick={() => setShowInfo(!showInfo)}
+              >
+                {showInfo ? <FaChevronUp /> : <FaChevronDown />}
+              </button>
+            </div>
+            {showInfo && <p className="info">{info}</p>}
           </article>
         );
       })}
